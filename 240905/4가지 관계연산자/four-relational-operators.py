0fb158338f2974c2c0b737b54944772efa47bcdb
sieun-baek <beehoney206@gmail.com>
2024-09-05 14:11:23 +0900

    Title: 4가지 관계연산자, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240905/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,20 @@
+n = input().split()
+a = int(n[0])
+b = int(n[1])
+
+if a >= b:
+    print(1)
+else:
+    print(0)
+if a > b:
+    print(1)
+else:
+    print(0)
+if a <= b:
+    print(1)
+else:
+    print(0)
+if a < b:
+    print(1)
+else:
+    print(0)
