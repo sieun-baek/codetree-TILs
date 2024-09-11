b71370d8c5f375ebd2f3239b3e102f0807196b58
sieun-baek <beehoney206@gmail.com>
2024-09-11 17:01:50 +0900

    Title: 최댓값 구하기, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240911/최댓값 구하기/maximum-value.py
+++ 240911/최댓값 구하기/maximum-value.py
@@ -3,9 +3,9 @@ a = int(n[0])
 b = int(n[1])
 c = int(n[2])
 
-if a > b and a > c:
+if a >= b and a >= c:
     print(a)
-elif b > a and b > c:
+elif b >= a and b >= c:
     print(b)
 else:
     print(c)
