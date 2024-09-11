38cb9ef5c712f23db8ea5430511171e379bdad96
sieun-baek <beehoney206@gmail.com>
2024-09-11 13:44:53 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240911/두 사람/two-person.py
+++ 240911/두 사람/two-person.py
@@ -6,7 +6,9 @@ ag = a[1]
 ba = int(b[0])
 bg = b[1]
 
-if (ag or bg) == "M" and (aa or ba) >= 19:
+if ag == "M" and aa >= 19:
+    print(1)
+elif bg == "M" and bg >= 19:
     print(1)
 else:
     print(0)
