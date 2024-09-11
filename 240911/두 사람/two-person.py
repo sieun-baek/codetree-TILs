9a14f156a5e5c00d7827cdc541604ad3f220b783
sieun-baek <beehoney206@gmail.com>
2024-09-11 14:13:51 +0900

    Title: 두 사람, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240911/두 사람/two-person.py
+++ 240911/두 사람/two-person.py
@@ -6,7 +6,7 @@ ag = a[1]
 ba = int(b[0])
 bg = b[1]
 
-if (ag == "M" and aa >= 19) or (bg == "M" and bg >= 19):
-    print(1)
-else:
+if (aa and ba) < 19 or (ag and bg) == "M":
     print(0)
+else:
+    print(1)
