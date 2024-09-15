2cfc64ec0caeaf17f34bc79672c32c3ea0790346
sieun-baek <beehoney206@gmail.com>
2024-09-15 20:24:06 +0900

    Title: 숫자의 증감, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240915/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,12 @@
+a = input().split()
+c = a[0]
+n = int(a[1])
+
+if c == 'A':
+    for i in range(1, n+1):
+        print(i, end = '')
+        i += 1
+else:
+    for i in range(n-1, 1):
+        print(n, end = '')
+        n -= 1
