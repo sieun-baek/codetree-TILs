bbd6bc2739d8ca5820edb305c4ccfc6d1399dbe3
sieun-baek <beehoney206@gmail.com>
2024-09-19 11:07:34 +0900

    Title: 369 게임, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240919/369 게임/369-game.py
+++ 240919/369 게임/369-game.py
@@ -2,11 +2,12 @@ n = int(input())
 
 for i in range(1, n+1):
     m = i % 10
+    j = i // 10
     if i % 3 == 0:
         print(0, end=' ')
     elif (m == 3) or (m == 6) or (m == 9):
         print(0, end=' ')
-    elif i // 10 == 3:
+    elif (j == 3) or (j == 6) or (j == 9):
         print(0, end=' ')
     else:
         print(i, end=' ')
