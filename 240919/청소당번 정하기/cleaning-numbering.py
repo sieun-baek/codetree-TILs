0e5be656aa6a21f26ea4a62dd4921ee92b36e740
sieun-baek <beehoney206@gmail.com>
2024-09-19 15:08:37 +0900

    Title: 청소당번 정하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240919/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,14 @@
+n = int(input())
+cnt1 = 0
+cnt2 = 0
+cnt3 = -1
+
+for i in range(n):
+    if i % 12 == 0:
+        cnt3 += 1
+    elif i % 3 == 0:
+        cnt2 += 1
+    elif i % 2 == 0:
+        cnt1 += 1
+
+print(cnt1, cnt2, cnt3)
