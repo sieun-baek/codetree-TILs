bf2133ffc923c5c34e4dd41aff84935ae85a3712
sieun-baek <beehoney206@gmail.com>
2024-09-19 11:06:36 +0900

    Title: 369 게임, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240919/369 게임/369-game.py
+++ 240919/369 게임/369-game.py
@@ -6,6 +6,8 @@ for i in range(1, n+1):
         print(0, end=' ')
     elif (m == 3) or (m == 6) or (m == 9):
         print(0, end=' ')
+    elif i / 10 == 3:
+        print(0, end=' ')
     else:
         print(i, end=' ')
     i += 1
