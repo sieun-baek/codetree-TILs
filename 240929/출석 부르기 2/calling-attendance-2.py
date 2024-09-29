6c9d8dbd60dd818ebfc88f0e9577c012dfe7a356
sieun-baek <beehoney206@gmail.com>
2024-09-29 16:17:44 +0900

    Title: 출석 부르기 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240929/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+i = 0
+
+while i <= 4:
+    i = int(input())
+    if i == 1:
+        print("John")
+    elif i == 2:
+        print("Tom")
+    elif i == 3:
+        print("Paul")
+    elif i == 4:
+        print("Sam")
+print("Vacancy")
